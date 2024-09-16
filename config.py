7f4737438c78707a79b2cf5586d5e1da3d350fcf
JuneKoo Kang <engineerjkk@naver.com>
2024-09-16 20:03:04 +0900

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+MY_OPENAI_API_KEY = "YOUR OPENAI API"
+MY_PINECONE_API_KEY = "YOUR PINECONE API"
+MY_IMG_MODEL = "gpt-4o-mini"
+MY_IMG_MODEL_HIGH = "gpt-4o"
+MY_TXT_MODEL = 'text-embedding-3-small'
+MY_GEN_MODEL = 'dall-e-3'
